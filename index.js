3416f3f1b9f245d89032a988701127b8ad43fd2b
Naveen Tehrpariya <naveentehrpariya@gmail.com>
2023-03-13 23:09:57 +0530

    with creadential added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
-app.use(cors({
-    origin:"http://localhost:3000/",
+app.use(cors({ 
+    origin: true, 
+    credentials: true 
 }));
+
 
 require('./db/config');
 app.use(express.json());
