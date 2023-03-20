68bdeb2b057d3b931d1c449e05897ed505228a12
Redzone <naveen@internetbusinesssolutionsindia.com>
2023-03-20 11:13:07 +0530

    changes off

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 require('./db/config');
 app.use(express.json());
+
 app.use(cors());
 
 const { login, signup } = require('./controllers/userController');
