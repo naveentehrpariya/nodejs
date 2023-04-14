a8342e302dbe2a8f879d355d71794d7055a134a6
Redzone <naveen@internetbusinesssolutionsindia.com>
2023-04-14 18:30:21 +0530

    api features implementation

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,6 +2,32 @@ const Products  = require("../db/Products");
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process && process.env.SECRET_ACCESS;
 
+
+class APIFeatures { 
+    constructor(query, queryString){ 
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    filter(){ 
+        const QueryObject  = {...this.queryString}
+
+        // REMOVE SORTING PAREMETER FROM QUERY
+        const excludeQueries = ['page', 'sort', 'limit', 'fields']
+        excludeQueries.forEach(el => delete QueryObject[el]);
+
+        // ORDER BY 
+        let querySting = JSON.stringify(QueryObject);
+        querySting = querySting.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        // JSON.parse(querySting)
+        this.query.find(JSON.parse(querySting));
+        return this;
+    }
+}
+
+
+
 const addproducts = async (req, res)=>{
     try {
         // const decodedToken = jwt.verify(req.headers.authorization.split(' ')[1], JWT_SECRET);
@@ -35,19 +61,20 @@ const listProducts = async (req, res)=>{
         // const { price } = req.query;
         // console.log(query);
 
-        const QueryObject  = {...req.query}
+        // const QueryObject  = {...req.query}
 
-        // REMOVE SORTING PAREMETER FROM QUERY
-        const excludeQueries = ['page', 'sort', 'limit', 'fields']
-        excludeQueries.forEach(el => delete QueryObject[el]);
+        // // REMOVE SORTING PAREMETER FROM QUERY
+        // const excludeQueries = ['page', 'sort', 'limit', 'fields']
+        // excludeQueries.forEach(el => delete QueryObject[el]);
 
-        // ORDER BY 
-        let querySting = JSON.stringify(QueryObject);
-        querySting = querySting.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        // // ORDER BY 
+        // let querySting = JSON.stringify(QueryObject);
+        // querySting = querySting.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-        // JSON.parse(querySting)
+        // // JSON.parse(querySting)
         let query = Products.find(JSON.parse(querySting));
-        console.log("query", req.query);
+        // console.log("query", req.query);
+        
 
         // SORTING
         if(req.query.sort){
@@ -66,7 +93,9 @@ const listProducts = async (req, res)=>{
         const skip = (page - 1) * limit;
         query.skip(skip).limit(limit);
 
-        const data = await query;
+        const feature = new APIFeatures(Products.find(), req.query).filter();
+        const data = await feature.query;
+
         if(data){ 
             res.json({ 
                 status:true, 
